d0dda4465af344e67639ccbb41ca2db9284d89e2
Niina Kristiina <niina.rantanen@metropolia.fi>
2016-04-08 10:00:29 +0300

    Initial Commit

---
+++ PolarisApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PolarisApp
+//
+//  Created by Niina Kristiina on 08/04/16.
+//  Copyright © 2016 Niina Kristiina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
