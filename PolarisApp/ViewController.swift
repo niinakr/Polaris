0742da99f2eb51f912c94748d6e691a7c41f2541
Niina Kristiina <niina.rantanen@metropolia.fi>
2016-04-10 19:52:24 +0300

    Copying things from Bang

--- PolarisApp/ViewController.swift
+++ PolarisApp/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import MapKit
+import CoreData
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func selfPosition(sender: AnyObject) {
+        
+        
+    }
+    
+    
+    @IBOutlet weak var searchView: UISearchBar!
+    
+    @IBOutlet weak var favouriteView: UIButton!
+    
+    @IBAction func favouriteAdd(sender: AnyObject) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
 
