4cec45276c863db95e33a1894d4a06926d899779
Niina Kristiina <niina.rantanen@metropolia.fi>
2016-04-22 20:06:17 +0300

    menubar

--- PolarisApp/menubar_beacon.swift
+++ PolarisApp/menubar_beacon.swift
@@ -8,8 +8,25 @@
 
 import Foundation
 
-class menubar_beacon: UIViewController {
+class menubar_beacon: UITableViewController {
+    var secondArray = [String]()
+    
+    
     override func viewDidLoad() {
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        secondArray = ["ice","blueberry","mint"]
     }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return secondArray.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell =  tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = secondArray[indexPath.row]
+        
+        return cell
+    }
+    
 }
